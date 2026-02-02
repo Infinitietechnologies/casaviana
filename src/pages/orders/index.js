e31e90b751ccbe12bdf202ad149e2198224292dc
harshad-pindoriya <infinitietechnologies05@gmail.com>
2026-02-02 13:20:27 +0530

    feat: Implement new `/orders` page for displaying user orders.

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -1,14 +1,15 @@
 import LayoutView from "@/views/LayoutView";
 import OrdersView from "@/views/OrdersView";
+import Head from "next/head";
 // import MetaTags from "@/components/MetaTags";
 
 export default function Orders() {
     return (
         <>
-            {/* <MetaTags
-                title="Meus Pedidos | Casa Viana"
-                description="Visualize o histórico de seus pedidos"
-            /> */}
+            <Head>
+                <title>Meus Pedidos | Casa Viana</title>
+                <meta name="description" content="Visualize o histórico de seus pedidos" />
+            </Head>
             <OrdersView />
             {/* <LayoutView>
             </LayoutView> */}
