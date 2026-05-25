07b1c5e0af683bbbf8391bd8d527374b69f134de
Harshad Pindoriya <89074260+harshad-pindoriya@users.noreply.github.com>
2026-05-25 14:04:55 +0530

    Delete src/pages/_middleware.js

--- src/pages/_middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  return new NextResponse('<h1>Site under maintenance</h1>', {
-    status: 503,
-    headers: { 'content-type': 'text/html' },
-  });
-}
